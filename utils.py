e5e211a5aa6c67070a3d48bce6554d120e5ece5e
aiwizzard <ajmalekvsroad@gmail.com>
2021-06-11 19:34:33 +0530

    utils for train

---
+++ utils.py
@@ -0,0 +1,23 @@
+import torch
+import numpy as np
+
+import config as config
+
+def create_masks(source, target, target_y, pad=0):
+    def subsequent_mask(size):
+        # mask = torch.triu(torch.ones(1, size, size)).type(dtype=torch.uint8)
+        # mask = mask.unsqueeze(0) == 0
+        # return mask
+        attn_shape = (1, size, size)
+        subsequent_mask = np.triu(np.ones(attn_shape), k=1).astype('uint8')
+        return torch.from_numpy(subsequent_mask) == 0
+
+    source_mask = (source != pad).to(config.device)
+
+    target_mask = target != pad
+    target_mask = target_mask.unsqueeze(-2) 
+    target_mask = target_mask & subsequent_mask(target.size(-1)).type_as(target_mask)
+
+    target_y_mask = target_y != pad 
+
+    return source_mask, target_mask, target_y_mask
