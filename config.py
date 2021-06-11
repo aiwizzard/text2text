5b4fffe78b892c6b11d8a491841b8aa6f095c6a3
aiwizzard <ajmalekvsroad@gmail.com>
2021-06-11 20:01:58 +0530

    debugging

--- config.py
+++ config.py
@@ -1,14 +1,22 @@
 import torch
 
-train_data = '.data/train_data.json'
+vocab_size = 30000
+max_len = 25
 batch_size = 64
 model_dim = 512
+ff_dim = 2048
 head = 8
 n_layers = 6
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+dropout_rate = 0.1
 epochs = 3
 
 lr = 1e-3
 betas = (0.9, 0.98)
 
 warmup = 4000
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+bert_model_name = 'bert-base-uncased'
+
+train_data = '.data/train_data.json'
--- train.py
+++ train.py
@@ -72,7 +72,8 @@ def train(epoch: int, config, model: ChatModel, data_loader, criterion, optimize
 
 
 def main(config):
-    train_data = json.load(config.train_data)
+    with open(config.train_data)as file:
+        train_data = json.load(file)
     train_loader = DataLoader(
         ChatDataSet(train_data),
         batch_size=config.batch_size,
