240bb0295db12f2eacabd5116a39d5feb3827f6b
aiwizzard <ajmalekvsroad@gmail.com>
2021-06-12 19:50:45 +0530

    debug

--- utils.py
+++ utils.py
@@ -10,10 +10,12 @@ def create_masks(source, target, target_y, pad=0):
         return mask
 
     source_mask = (source != pad).to(config.device)
+    source_mask = source_mask.unsqueeze(1).unsqueeze(1)
 
     target_mask = target != pad
-    target_mask = target_mask.unsqueeze(-2) 
-    target_mask = target_mask & subsequent_mask(target.size(-1)).type_as(target_mask)
+    target_mask = target_mask.unsqueeze(1) 
+    target_mask = target_mask & subsequent_mask(target.size(-1)).type_as(target_mask.data)
+    target_mask = target_mask.unsqueeze(1)
 
     target_y_mask = target_y != pad 
 
