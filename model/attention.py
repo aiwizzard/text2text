85d4edcb1ab57e20d287d9b5f9f246bd98710c92
aiwizzard <ajmalekvsroad@gmail.com>
2021-06-13 13:17:41 +0530

    formatted

--- model/attention.py
+++ model/attention.py
@@ -7,6 +7,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 class SelfAttention(nn.Module):
     def __init__(self, model_dim=512, head=8, dropout_rate=0.1):
         super(SelfAttention, self).__init__()
@@ -15,10 +16,11 @@ class SelfAttention(nn.Module):
         self.mult_attn = MultiHeadAttention(model_dim, head, dropout_rate)
 
     def forward(self, x, target_mask):
-        out = self.layer_norm(x)            
+        out = self.layer_norm(x)
         out = self.mult_attn(out, out, out, target_mask)
         out = self.dropout(out)
         return out + x
+
 
 class SourceTargetAttention(nn.Module):
     def __init__(self, model_dim=512, head=8, dropout_rate=0.1):
@@ -32,18 +34,19 @@ class SourceTargetAttention(nn.Module):
         out = self.mult_attn(out, mem, mem, source_mask)
         out = self.dropout(out)
         return out + x
-      
+
+
 class MultiHeadAttention(nn.Module):
     def __init__(self, model_dim=512, head=8, dropout_rate=0.1):
         super(MultiHeadAttention, self).__init__()
-        # assert that the model dimension deviced 
+        # assert that the model dimension deviced
         # by the number of heads is an integer value
         assert model_dim % head == 0, "invalid model dimension or number of heads"
         self.head = head
         self.key_dim = model_dim // self.head
 
         # To apply the linear transformation to the incomming data
-        self.query_weights  = nn.Linear(model_dim, model_dim)
+        self.query_weights = nn.Linear(model_dim, model_dim)
         self.key_weights = nn.Linear(model_dim, model_dim)
         self.value_weights = nn.Linear(model_dim, model_dim)
 
@@ -56,21 +59,35 @@ class MultiHeadAttention(nn.Module):
         if mask is not None:
             mask = mask.unsqueeze(1)
 
-        query = self.query_weights(query).contiguous().view(batch_size, -1, self.head, \
-            self.key_dim).transpose(1, 2)
-        key = self.key_weights(key).contiguous().view(batch_size, -1, self.head, \
-            self.key_dim).transpose(1, 2)
-        value = self.value_weights(value).contiguous().view(batch_size, -1, self.head, \
-            self.key_dim).transpose(1, 2)
+        query = (
+            self.query_weights(query)
+            .contiguous()
+            .view(batch_size, -1, self.head, self.key_dim)
+            .transpose(1, 2)
+        )
+        key = (
+            self.key_weights(key)
+            .contiguous()
+            .view(batch_size, -1, self.head, self.key_dim)
+            .transpose(1, 2)
+        )
+        value = (
+            self.value_weights(value)
+            .contiguous()
+            .view(batch_size, -1, self.head, self.key_dim)
+            .transpose(1, 2)
+        )
 
         out, attention = self.attention(query, key, value, mask)
-        # Transpose to move the head dimension back and 
+        # Transpose to move the head dimension back and
         # combine the last two dimensions to concatenate all the heads together
-        out = out.transpose(1, 2).contiguous().view(batch_size, \
-            -1, self.head * self.key_dim)
+        out = (
+            out.transpose(1, 2)
+            .contiguous()
+            .view(batch_size, -1, self.head * self.key_dim)
+        )
 
         return self.dense(out)
-        
 
 
 class ScaledDotProductAttention(nn.Module):
@@ -79,7 +96,7 @@ class ScaledDotProductAttention(nn.Module):
         self.dropout_rate = dropout_rate
         self.layer_norm = nn.LayerNorm(model_dim)
         self.dropout = nn.Dropout(dropout_rate)
-        
+
     def forward(self, query, key, value, mask=None):
         dk = query.size(-1)
         attention = torch.matmul(query, key.transpose(-2, -1)) / math.sqrt(dk)
