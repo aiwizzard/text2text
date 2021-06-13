a31d6325bbb6fa4b6b1161c296bdf7dd473f024d
aiwizzard <ajmalekvsroad@gmail.com>
2021-06-13 13:46:45 +0530

    debug

--- train.py
+++ train.py
@@ -43,9 +43,8 @@ def train(epoch: int, config, model: ChatModel, data_loader, criterion, optimize
             out = model(x, source_mask, target, target_mask)
 
             optimizer.zero_grad()
-            print(f"out: {out.shape}, target: {target_y.shape}")
 
-            loss = criterion(out, target_y)
+            loss = criterion(out.transpose(1, 2), target_y).mean()
             loss.backward()
             optimizer.step()
 
