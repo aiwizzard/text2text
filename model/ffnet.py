468ccdb1b54a0b368b18bd2a7ab4df3c9514a411
aiwizzard <ajmalekvsroad@gmail.com>
2021-06-18 00:12:11 +0530

    moved to conv1d

--- model/ffnet.py
+++ model/ffnet.py
@@ -3,16 +3,17 @@ import torch.nn as nn
 class FFNet(nn.Module):
     def __init__(self, model_dim=512, ff_dim=2048, dropout_rate=0.1):
         super(FFNet, self).__init__()
-        self.dense1 = nn.Linear(model_dim, ff_dim)
-        self.dense2 = nn.Linear(ff_dim, model_dim)
+        self.layer1 = nn.Conv1d(model_dim, ff_dim, 1)
+        self.layer2 = nn.Conv1d(ff_dim, model_dim, 1)
         self.relu = nn.ReLU()
         self.layer_norm = nn.LayerNorm(model_dim)
         self.dropout = nn.Dropout(dropout_rate)
 
     def forward(self, x):
-        x = self.dense1(x)
+        x = self.layer_norm(x)
+        x = self.layer1(x)
         x = self.relu(x)
-        x = self.dense2(x)
+        x = self.layer1(x)
         x = self.dropout(x)
         x = self.layer_norm(x)
         return x
