9fba8558ff42c489f291d759af3be2e1bfff3d40
aiwizzard <ajmalekvsroad@gmail.com>
2021-06-18 00:11:47 +0530

    masks

--- utils.py
+++ utils.py
@@ -3,19 +3,14 @@ import numpy as np
 
 import config as config
 
+def subsequent_mask(size):
+    mask = np.triu(np.ones(1, size, size), k=1).astype('uint8')
+    return torch.from_numpy(mask) == 0
+
 def create_masks(source, target, target_y, pad=0):
-    def subsequent_mask(size):
-        mask = torch.triu(torch.ones(size, size)).transpose(0, 1).type(dtype=torch.uint8)
-        mask = mask.unsqueeze(0)
-        return mask
-
     source_mask = (source != pad).to(config.device)
-    source_mask = source_mask
 
-    target_mask = target != pad
-    target_mask = target_mask.unsqueeze(1) 
-    target_mask = target_mask & subsequent_mask(target.size(-1)).type_as(target_mask.data)
-
-    target_y_mask = target_y != pad 
-
-    return source_mask, target_mask, target_y_mask
+    target_mask = (target != pad).unsqueeze(-2)
+    target_mask = target_mask & subsequent_mask(target.size(-1)).type_as(target_mask)
+    
+    return source_mask, target_mask
