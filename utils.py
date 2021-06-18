08c07e59cb24fa4f2b6e5717287f7224f86ecdda
aiwizzard <ajmalekvsroad@gmail.com>
2021-06-18 10:49:14 +0530

    temp

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import torch
 import numpy as np
+import pickle
+from tqdm import tqdm
 
 import config as config
 
@@ -15,3 +17,17 @@ def create_masks(source, target, pad=0):
     target_mask = target_mask & subsequent_mask(target.size(-1)).type_as(target_mask)
 
     return source_mask, target_mask
+
+def create_train_data(file_path: str, tokenizer, use_pickle=False) -> list:
+    data = []
+    if use_pickle:
+        with open(file_path, 'rb') as file:
+            data = pickle.load(file)
+    else:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            lines = file.readlines()
+        for i in tqdm(range(0, len(lines), 3)):
+            data.append(tuple(map(tokenizer.convert, lines[i: i+2])))
+        with open('.data/train_data.pkl', 'wb') as file:
+            pickle.dump(data, file)
+    return data
